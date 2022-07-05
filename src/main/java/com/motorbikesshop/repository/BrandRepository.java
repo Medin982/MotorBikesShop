df1044eaca2b495d568fa6da38d573eb3a53c6a0
Medin Tarakchiev <86888951+medin982@users.noreply.github.com>
2022-07-05 19:06:18 +0300

    remove

--- src/main/java/com/motorbikesshop/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.motorbikesshop.repository;
-
-import com.motorbikesshop.model.entity.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrandRepository extends JpaRepository<Brand, String> {
-}
